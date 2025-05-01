8d6e9151b17ac7ae1059c4afdeb0fb47498bed21
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-01 06:19:55 +0100

    Update HomeScreen.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/screens/HomeScreen.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/screens/HomeScreen.kt
@@ -1,1 +1,15 @@
+package com.gibson.fobicx.screens
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+
+@Composable
+fun HomeScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Home Screen")
+    }
+}
+
+// Do the same for MarketplaceScreen, PostScreen, ChatScreen, and ProfileScreen
 // HomeScreen.kt
