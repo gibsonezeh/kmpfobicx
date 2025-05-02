02c28a884acdc107248676da962fea5fbc3674dd
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-02 22:30:20 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
@@ -4,34 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.gibson.fobicx.navigation.BottomNavBar
 import com.gibson.fobicx.navigation.NavigationGraph
+import com.gibson.fobicx.ui.ThemePreferenceManager
 import com.gibson.fobicx.ui.theme.FobicxTheme
-import com.gibson.fobicx.utils.ThemePreferenceManager
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            var isDarkTheme by remember { mutableStateOf(false) }
-            val scope = rememberCoroutineScope()
 
-            // Load saved theme preference on launch
+        setContent {
+            var isDarkMode by remember { mutableStateOf(false) }
+
+            // Read dark mode setting when app starts
             LaunchedEffect(Unit) {
-                isDarkTheme = ThemePreferenceManager.isDarkMode(applicationContext)
+                isDarkMode = withContext(Dispatchers.IO) {
+                    ThemePreferenceManager.isDarkMode(applicationContext)
+                }
             }
 
-            FobicxTheme(darkTheme = isDarkTheme) {
+            FobicxTheme(darkTheme = isDarkMode) {
                 val navController = rememberNavController()
                 Scaffold(
-                    bottomBar = {
-                        BottomNavBar(navController)
-                    }
+                    bottomBar = { BottomNavBar(navController) }
                 ) { innerPadding ->
                     NavigationGraph(
                         navController = navController,
