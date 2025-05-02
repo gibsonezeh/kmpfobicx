0485a3658e2e1dc57a6fd378887a4a105a22ab6d
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-02 22:32:34 +0100

    Update Theme.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/Theme.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/Theme.kt
@@ -3,24 +3,17 @@ package com.gibson.fobicx.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Color(0xFF1DB954),
-    onPrimary = Color.Black,
-    background = Color(0xFF121212),
-    onBackground = Color.White,
-    surface = Color(0xFF1E1E1E),
-    onSurface = Color.White
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Color(0xFF1DB954),
-    onPrimary = Color.White,
-    background = Color.White,
-    onBackground = Color.Black,
-    surface = Color(0xFFF5F5F5),
-    onSurface = Color.Black
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 )
 
 @Composable
@@ -32,7 +25,7 @@ fun FobicxTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography(),
+        typography = Typography,
         content = content
     )
 }
