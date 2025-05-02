3c1d19e00994197ff22c135b7666e959d8c01bd9
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-02 20:09:45 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
@@ -11,12 +11,22 @@ import androidx.navigation.compose.rememberNavController
 import com.gibson.fobicx.navigation.BottomNavBar
 import com.gibson.fobicx.navigation.NavigationGraph
 import com.gibson.fobicx.ui.theme.FobicxTheme
+import com.gibson.fobicx.utils.ThemePreferenceManager
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FobicxTheme {
+            var isDarkTheme by remember { mutableStateOf(false) }
+            val scope = rememberCoroutineScope()
+
+            // Load saved theme preference on launch
+            LaunchedEffect(Unit) {
+                isDarkTheme = ThemePreferenceManager.isDarkMode(applicationContext)
+            }
+
+            FobicxTheme(darkTheme = isDarkTheme) {
                 val navController = rememberNavController()
                 Scaffold(
                     bottomBar = {
