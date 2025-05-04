e27a16ac4b73c1befe4d14fe7bb7b881eda5dac8
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-04 22:42:05 +0100

    Update MainScreen.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/screens/MainScreen.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/screens/MainScreen.kt
@@ -4,22 +4,36 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.*
+import com.gibson.fobicx.navigation.Screen
 import com.gibson.fobicx.ui.components.BottomNavBar
+import com.gibson.fobicx.ui.screens.pages.*
 
 @Composable
 fun MainScreen() {
-    var selected by remember { mutableStateOf("Home") }
+    val navController = rememberNavController()
 
     Scaffold(
         bottomBar = {
             BottomNavBar { clicked ->
-                selected = clicked
+                navController.navigate(clicked) {
+                    popUpTo(navController.graph.startDestinationId) { saveState = true }
+                    launchSingleTop = true
+                    restoreState = true
+                }
             }
         }
     ) { innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)) {
-            Text(text = "Current: $selected", modifier = Modifier.padding(16.dp))
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) { HomeScreen() }
+            composable(Screen.Materials.route) { MaterialsScreen() }
+            composable(Screen.Post.route) { PostScreen() }
+            composable(Screen.Stock.route) { StockScreen() }
+            composable(Screen.Me.route) { ProfileScreen() }
         }
     }
 }
