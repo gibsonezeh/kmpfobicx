e3e117ea4c1240c2c64b8b715cd0b36e94b6ed73
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-04 13:57:38 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
@@ -4,38 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.gibson.fobicx.navigation.BottomNavBar
 import com.gibson.fobicx.navigation.NavigationGraph
 import com.gibson.fobicx.ui.theme.FobicxTheme
-import com.gibson.fobicx.ui.ThemePreferenceManager
-import kotlinx.coroutines.launch
+import com.gibson.fobicx.util.DarkModeManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val isDark = DarkModeManager.isDarkMode(applicationContext)
+
         setContent {
-            var isDarkTheme by remember { mutableStateOf(false) }
-            val scope = rememberCoroutineScope()
+            var isDarkMode by remember { mutableStateOf(isDark) }
 
-            // Load saved theme preference on launch
-            LaunchedEffect(Unit) {
-                isDarkTheme = ThemePreferenceManager.isDarkMode(applicationContext)
-            }
-
-            FobicxTheme(darkTheme = isDarkTheme) {
+            FobicxTheme(darkTheme = isDarkMode) {
                 val navController = rememberNavController()
                 Scaffold(
-                    bottomBar = {
-                        BottomNavBar(navController)
-                    }
+                    bottomBar = { BottomNavBar(navController) }
                 ) { innerPadding ->
                     NavigationGraph(
                         navController = navController,
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        onToggleTheme = { enabled ->
+                            isDarkMode = enabled
+                            DarkModeManager.setDarkMode(applicationContext, enabled)
+                        }
                     )
                 }
             }
