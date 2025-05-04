20be98e83b978a37e30ae574eec3a90c6d122bae
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-04 21:42:59 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
@@ -3,38 +3,15 @@ package com.gibson.fobicx
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.gibson.fobicx.navigation.BottomNavBar
-import com.gibson.fobicx.navigation.NavigationGraph
+import com.gibson.fobicx.ui.screens.MainScreen
 import com.gibson.fobicx.ui.theme.FobicxTheme
-import com.gibson.fobicx.util.DarkModeManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val isDark = DarkModeManager.isDarkMode(applicationContext)
-
         setContent {
-            var isDarkMode by remember { mutableStateOf(isDark) }
-
-            FobicxTheme(darkTheme = isDarkMode) {
-                val navController = rememberNavController()
-                Scaffold(
-                    bottomBar = { BottomNavBar(navController) }
-                ) { innerPadding ->
-                    NavigationGraph(
-                        navController = navController,
-                        modifier = Modifier.padding(innerPadding),
-                        onToggleTheme = { enabled ->
-                            isDarkMode = enabled
-                            DarkModeManager.setDarkMode(applicationContext, enabled)
-                        }
-                    )
-                }
+            FobicxTheme {
+                MainScreen()
             }
         }
     }
