f100f022af3ec98f6c26cd3cf5e2669ac9e0778a
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-04 22:52:34 +0100

    Create ProfileScreen.kt

---
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/screens/pages/ProfileScreen.kt
@@ -0,0 +1,11 @@
+package com.gibson.fobicx.ui.screens.pages
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProfileScreen() {
+    Text("Profile", modifier = Modifier.padding(16.dp))
+}
