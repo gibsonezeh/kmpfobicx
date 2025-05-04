edd67727ec59a0b24034ba0ac2a2bf44911e99b4
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-04 14:10:05 +0100

    Update Color.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/Color.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/Color.kt
@@ -1,13 +1,30 @@
 package com.gibson.fobicx.ui.theme
 
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
-// Light Theme Colors
-val Purple40 = Color(0xFF6750A4)
-val PurpleGrey40 = Color(0xFF625B71)
-val Pink40 = Color(0xFF7D5260)
+// Define your color palette here
 
-// Dark Theme Colors
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple40 = Color(0xFF6A1B9A)
+val PurpleGrey40 = Color(0xFF6A4D99)
+val Pink40 = Color(0xFFEC407A)
+
+val Purple80 = Color(0xFF9C4DFF)
+val PurpleGrey80 = Color(0xFF7E57C2)
+val Pink80 = Color(0xFFF06292)
+
+// Light theme color scheme
+val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
+
+// Dark theme color scheme
+val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
