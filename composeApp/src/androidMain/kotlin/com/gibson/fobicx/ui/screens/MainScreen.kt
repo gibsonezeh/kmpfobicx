e72fffd806b6f78d695b981ac812700dbb152981
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-07 20:51:48 +0100

    Update MainScreen.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/screens/MainScreen.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/screens/MainScreen.kt
@@ -1,1 +1,53 @@
+package com.gibson.fobicx.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.*
+import com.gibson.fobicx.navigation.Screen
+import com.gibson.fobicx.ui.components.BottomNavBar
+import com.gibson.fobicx.ui.screens.pages.*
+
+@Composable
+fun MainScreen(onLogout: () -> Unit = {}) {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 4.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Box(
+                    modifier = Modifier.widthIn(max = 500.dp)
+                ) {
+                    BottomNavBar(onItemClick =  { clicked ->
+                        navController.navigate(clicked) {
+                            popUpTo(navController.graph.startDestinationId) { saveState = true }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    })
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) { HomeScreen() }
+            composable(Screen.Materials.route) { MarketScreen() }
+            composable(Screen.Post.route) { PostScreen() }
+            composable(Screen.Stock.route) { StockScreen() }
+            composable(Screen.Me.route) { ProfileScreen(onLogout = onLogout) }
+        }
+    }
+}
 
