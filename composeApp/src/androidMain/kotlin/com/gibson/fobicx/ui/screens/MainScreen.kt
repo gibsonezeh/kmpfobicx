8aa37dd843605b4bf77ec849124db163795634fa
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-09 21:23:40 +0100

    Update MainScreen.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/screens/MainScreen.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/ui/screens/MainScreen.kt
@@ -1,4 +1,4 @@
-package com.gibson.fobicx.presentation.screens
+package com.gibson.fobicx.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
@@ -7,15 +7,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.*
-import com.gibson.fobicx.navigation.Screen
-import com.gibson.fobicx.presentation.components.BottomNavBar
+import com.gibson.fobicx.ui.navigation.Screen
+import com.gibson.fobicx.ui.components.BottomNavBar
 import com.gibson.fobicx.ui.screens.pages.ProfileScreen
 
 @Composable
 fun MainScreen(onLogout: () -> Unit = {}) {
     val navController = rememberNavController()
-
-    // Theme toggle state
     var isDarkTheme by remember { mutableStateOf(false) }
 
     Scaffold(
@@ -26,9 +24,7 @@ fun MainScreen(onLogout: () -> Unit = {}) {
                     .padding(bottom = 4.dp),
                 contentAlignment = Alignment.Center
             ) {
-                Box(
-                    modifier = Modifier.widthIn(max = 500.dp)
-                ) {
+                Box(modifier = Modifier.widthIn(max = 500.dp)) {
                     BottomNavBar { clicked ->
                         navController.navigate(clicked) {
                             popUpTo(navController.graph.startDestinationId) { saveState = true }
