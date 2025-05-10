9b9d817b93fec9ee31e39badeca8d18474230571
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-10 22:27:15 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
@@ -3,21 +3,18 @@ package com.gibson.fobicx
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.gibson.fobicx.navigation.AppNavigation
 import com.gibson.fobicx.ui.theme.FobicxTheme
+import com.gibson.fobicx.viewmodel.ThemeViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var isDarkTheme by remember { mutableStateOf(false) }
-
-            FobicxTheme(darkTheme = isDarkTheme) {
-                AppNavigation(
-                    isDarkTheme = isDarkTheme,
-                    onToggleTheme = { isDarkTheme = !isDarkTheme }
-                )
+            val themeViewModel: ThemeViewModel = viewModel()
+            FobicxTheme(darkTheme = themeViewModel.isDarkTheme.value) {
+                AppNavigation(themeViewModel)
             }
         }
     }
