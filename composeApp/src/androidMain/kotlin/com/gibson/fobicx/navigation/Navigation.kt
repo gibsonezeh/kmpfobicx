f6546c075e5d26e2f05b49bbc95188a000032a37
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-10 23:03:33 +0100

    Update Navigation.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/navigation/Navigation.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/navigation/Navigation.kt
@@ -1,6 +1,8 @@
 package com.gibson.fobicx.navigation
 
 sealed class Screen(val route: String) {
+    object Login : Screen("login")
+    object Signup : Screen("signup")
     object Home : Screen("home")
     object Materials : Screen("market")
     object Post : Screen("post")
