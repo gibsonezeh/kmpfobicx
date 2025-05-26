a0fec59c29acf9244257f5c1e4162b539e4cfdf4
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-05-27 00:02:24 +0100

    Update AppNavigation.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/navigation/AppNavigation.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/navigation/AppNavigation.kt
@@ -1,72 +1,48 @@
-package com.gibson.fobicx.navigation
+// AppNavigation.kt package com.gibson.fobicx.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavType
-import androidx.navigation.compose.*
-import com.gibson.fobicx.ui.screens.HomeScreen
-import com.gibson.fobicx.ui.screens.ProfileScreen
-import com.gibson.fobicx.ui.screens.auth.AccountSetupScreen
-import com.gibson.fobicx.ui.screens.auth.PhoneVerificationScreen
-import com.gibson.fobicx.ui.screens.auth.SignupScreen
-import com.gibson.fobicx.viewmodel.AuthViewModel
-import com.gibson.fobicx.viewmodel.ProfileViewModel
+import androidx.compose.runtime.Composable import androidx.lifecycle.viewmodel.compose.viewModel import androidx.navigation.NavHostController import androidx.navigation.compose.NavHost import androidx.navigation.compose.composable import androidx.navigation.compose.rememberNavController import com.gibson.fobicx.ui.screens.* import com.gibson.fobicx.ui.screens.auth.* import com.gibson.fobicx.viewmodel.AuthViewModel
 
-object Routes {
-    const val SIGNUP = "signup"
-    const val ACCOUNT_SETUP = "account_setup"
-    const val PHONE_VERIFICATION = "phone_verification"
-    const val HOME = "home"
-    const val PROFILE = "profile"
-}
+object Routes { const val LOGIN = "login" const val SIGNUP = "signup" const val ACCOUNT_SETUP = "account_setup" const val PHONE_VERIFICATION = "phone_verification" const val HOME = "home" }
 
-@Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
-    val authViewModel: AuthViewModel = viewModel()
-    val profileViewModel: ProfileViewModel = viewModel()
+@Composable fun AppNavigation() { val navController = rememberNavController() val authViewModel: AuthViewModel = viewModel()
 
-    NavHost(navController = navController, startDestination = Routes.SIGNUP) {
-        composable(Routes.SIGNUP) {
-            SignupScreen(
-                onSignupSuccess = {
-                    navController.navigate(Routes.ACCOUNT_SETUP)
-                }
-            )
-        }
-        composable(Routes.ACCOUNT_SETUP) {
-            AccountSetupScreen(
-                onSkip = {
-                    navController.navigate(Routes.PHONE_VERIFICATION)
-                },
-                onContinue = {
-                    navController.navigate(Routes.PHONE_VERIFICATION)
-                }
-            )
-        }
-        composable(Routes.PHONE_VERIFICATION) {
-            PhoneVerificationScreen(
-                onSkip = {
-                    navController.navigate(Routes.HOME) {
-                        popUpTo(Routes.SIGNUP) { inclusive = true }
-                    }
-                },
-                onVerified = {
-                    navController.navigate(Routes.HOME) {
-                        popUpTo(Routes.SIGNUP) { inclusive = true }
-                    }
-                }
-            )
-        }
-        composable(Routes.HOME) {
-            HomeScreen(
-                onProfileClick = {
-                    navController.navigate(Routes.PROFILE)
-                }
-            )
-        }
-        composable(Routes.PROFILE) {
-            ProfileScreen()
-        }
+val startDestination = if (authViewModel.isLoggedIn()) Routes.HOME else Routes.LOGIN
+
+NavHost(navController = navController, startDestination = startDestination) {
+    composable(Routes.LOGIN) {
+        LoginScreen(
+            onLoginSuccess = { navController.navigate(Routes.HOME) { popUpTo(Routes.LOGIN) { inclusive = true } } },
+            onNavigateToSignup = { navController.navigate(Routes.SIGNUP) }
+        )
+    }
+
+    composable(Routes.SIGNUP) {
+        SignupScreen(
+            onSignupSuccess = { navController.navigate(Routes.ACCOUNT_SETUP) },
+            onNavigateToLogin = { navController.navigate(Routes.LOGIN) }
+        )
+    }
+
+    composable(Routes.ACCOUNT_SETUP) {
+        AccountSetupScreen(
+            onContinue = { navController.navigate(Routes.PHONE_VERIFICATION) },
+            onSkip = { navController.navigate(Routes.HOME) }
+        )
+    }
+
+    composable(Routes.PHONE_VERIFICATION) {
+        PhoneVerificationScreen(
+            onVerified = { navController.navigate(Routes.HOME) },
+            onSkip = { navController.navigate(Routes.HOME) }
+        )
+    }
+
+    composable(Routes.HOME) {
+        MainScreen()
     }
 }
+
+}
+
+// Other screens (SignupScreen, LoginScreen, AccountSetupScreen, etc.) will be added next one by one.
+
